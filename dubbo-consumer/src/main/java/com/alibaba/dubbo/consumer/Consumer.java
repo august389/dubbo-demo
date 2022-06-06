07b43b5ad91c1969c5190096356623665b6aa63d
TuNguyen <ituet1119@gmail.com>
2022-06-07 00:32:31 +0700

    complete deploy snapshot, run provider

---
+++ dubbo-consumer/src/main/java/com/alibaba/dubbo/consumer/Consumer.java
@@ -0,0 +1,19 @@
+package com.alibaba.dubbo.consumer;
+
+import com.alibaba.dubbo.demo.DemoService;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Consumer {
+
+    public static void main(String[] args) {
+
+        ClassPathXmlApplicationContext context =
+                new ClassPathXmlApplicationContext("consumer.xml");
+        context.start();
+        System.out.println("consumer start");
+        DemoService demoService= context.getBean(DemoService.class);
+        System.out.println("consumer");
+        System.out.println(demoService.getPermissions(1L));
+
+    }
+}
